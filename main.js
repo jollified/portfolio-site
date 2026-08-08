59d500f789aa9761ade824956ec4c0a0147e41b6
jollified <jolliestburger@gmail.com>
2026-08-08 15:10:04 -0600

    added basic cmd

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var c = false
 const cursor = document.getElementById("blinking-cursor");
-
+const stdout = document.getElementById("cmd-output")
 
 setInterval(cursor_blink, 1000);
 function cursor_blink() {
