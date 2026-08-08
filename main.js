74cf3a65d6280d4692b89ce241100a0bbcfb7b43
jollified <jolliestburger@gmail.com>
2026-08-08 00:06:47 -0600

    gvv

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var c = false
 const cursor = document.getElementById("blinking-cursor");
 
-setInterval(cursor_blink, 530)
 
+setInterval(cursor_blink, 1000);
 function cursor_blink() {
     c = !c
 
     if (c == true) {
         cursor.style.backgroundColor = "#00000000";
     } else {
-        cursor.style.backgroundColor = "#ffffff7f";
+        cursor.style.backgroundColor = "#ffffffaf";
     }
 }
+
